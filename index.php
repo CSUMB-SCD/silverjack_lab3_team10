c2062f4988d751c62b9dfb8ad18081d83ab1ed03
Mathias Torgalsboeen <mtorgalsboeen@csumb.edu>
2018-02-15 01:16:01 +0000

    Added my first commit

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
     </head>
-    
     <body>
         <?php
-            echo "<img src='./img/cards/clubs/2.png' />";
+            $player1 = array(
+                'name' => 'Mathias',
+                'imgURL' => '/profile_pictures/koala.jpg',
+                'hand' => array(),
+                'points' => 0
+            );
+            function printGameState($allPlayers){
+                foreach ($allPlayers as $player) {
+                    echo $player['name'] . "<br>";
+                }
+            }    
+            printGameState($allPlayers);
         ?>
     </body>
-    
 </html>
