f788731046bae49523a0d950d6d10743292cd794
Joshua Saavedra <jsaavedra@csumb.edu>
2018-02-15 01:28:32 +0000

    Added my picture and player2

--- index.php
+++ index.php
@@ -10,11 +10,20 @@
                 'hand' => array(),
                 'points' => 0
             );
+            
+            $player2 = array(
+                'name' => 'Josh',
+                'imgURL' => './img/profile_pictures/josh.jpg',
+                'hand' => array(),
+                'points' => 0
+            );
+            
             function printGameState($allPlayers){
                 foreach ($allPlayers as $player) {
                     echo $player['name'] . "<br>";
                 }
             }    
+            
             printGameState($allPlayers);
         ?>
     </body>
