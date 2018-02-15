420ce0f3be987c127bc540b0a497c53d980d0a0e
Joshua Saavedra <jsaavedra@csumb.edu>
2018-02-15 01:13:48 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    
+    <body>
+        <?php
+            echo "<img src='./img/cards/clubs/2.png' />";
+        ?>
+    </body>
+    
+</html>
