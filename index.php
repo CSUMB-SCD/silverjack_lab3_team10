501032abe7809208cea4d880e84ced505827fb8a
Raju Kumar <rakumar@csumb.edu>
2018-02-17 19:24:58 +0000

    added cs rules path

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>SiverJack</title>
+        <link href = "css/styles.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <?php
