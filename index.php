2261f6a898c931d68ae4c55003cbbead953edede
Mathias Torgalsboeen <mtorgalsboeen@csumb.edu>
2018-02-17 20:43:50 +0000

    Added logiq to get to cards

--- index.php
+++ index.php
@@ -35,6 +35,57 @@
                 'points' => 0
                 );
                 
+            function play(){
+            for($i=0; $i<2; $i++){
+                ${randomValue . $i } = rand(0,12);
+                ${folder . $i } = rand(0,3);
+                displaySymbol(${randomValue . $i }, ${folder . $i });
+            }
+            points($randomValue0, $randomValue1);
+            }
+            
+            function displaySymbol($randomValue, $folder) {
+            switch ($folder) {
+                case 0: $folder = "clubs";
+                        break;
+                case 1: $folder = "diamonds";
+                        break;
+                case 2: $folder = "hearts";
+                        break;
+                case 3: $folder = "spades";
+                        break;
+            }
+            switch ($randomValue){
+                case 0: $symbol = "1";
+                        break;
+                case 1: $symbol = "2";
+                        break;
+                case 2: $symbol = "3";
+                        break;
+                case 3: $symbol = "4"; 
+                        break;
+                case 4: $symbol = "5";
+                        break;
+                case 5: $symbol = "6";
+                        break;
+                case 6: $symbol = "7";
+                        break;
+                case 7: $symbol = "8";
+                        break;
+                case 8: $symbol = "9"; 
+                        break;
+                case 9: $symbol = "10";
+                        break;
+                case 10: $symbol = "11";
+                        break;
+                case 11: $symbol = "12";
+                        break;
+                case 12: $symbol = "13";
+                        break;
+            }
+            echo "<img id='reel$pos' src='img/cards/$folder/$symbol.png' alt='$symbol' title='". ucfirst($symbol) . "' width='70' >";
+            }
+            play();
             
          /*   function printGameState($allPlayers){
                 foreach ($allPlayers as $player) {
@@ -59,7 +110,7 @@
                 }
             } */
             
-            printGameState($allPlayers);
+            //printGameState($allPlayers);
         ?>
         <div class="footer">
   			<p>Copyright @ Team 10 - CST 336 2018</p> <!-- Copyright @ Team 10 - CST336 2018 --> 
