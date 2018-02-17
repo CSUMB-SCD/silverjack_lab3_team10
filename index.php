82b0d427255b15044a52db04c04ccc3e0b35280e
Mayra Ochoa <mayochoa@csumb.edu>
2018-02-17 22:10:24 +0000

    Updated CSS

--- index.php
+++ index.php
@@ -4,48 +4,66 @@
         <title>SiverJack</title>
         <link href = "css/styles.css" rel="stylesheet" type="text/css" />
     </head>
+    <header>
+            <strong>
+                <h1>Silverjack</h1>
+            </strong>
+    </header>
     <body>
         <?php
             $player1 = array(
                 'name' => 'Mathias',
-                'imgURL' => '/profile_pictures/mathias.jpg',
+                'imgURL' => './img/profiles_pictures/mathias.jpg',
                 'hand' => array(),
                 'points' => 0
             );
             
             $player2 = array(
                 'name' => 'Josh',
-                'imgURL' => './img/profile_pictures/josh.jpg',
+                'imgURL' => './img/profiles_pictures/josh.jpg',
                 'hand' => array(),
                 'points' => 0
             );
             $player3 = array(
                 'name' => 'Raju',
-                'imgURL' => './img/profile_pictures/rk.png',
+                'imgURL' => './img/profiles_pictures/rk.png',
                 'hand' => array(),
                 'points' => 0
-                );
+            );
             $player4 = array(
                 'name' => 'Mayra',
-                'imgURL' => './img/profile_pictures/mayra.JPG',
+                'imgURL' => './img/profiles_pictures/mayra.JPG',
                 'hand' => array(),
                 'points' => 0
-                );
+            );
+                $allPlayers = array(
+                    $player1,
+                    $player2,
+                    $player3,
+                    $player4
+                    );
                 
             
-         /*   function printGameState($allPlayers){
+            function  printGameState($allPlayers){
+                $i=0;
+                
                 foreach ($allPlayers as $player) {
-                    echo $player['name'] . "<br>";
+                    echo "<img id ='reel$i' src='" . $player['imgURL'] . "' />";
+                    echo "<br/>";
+                    echo $player['name'] . "<br/>";
+                    $i++;
                 }
+                
             }
+            printGameState($allPlayers);
+            getImgURlForCardIndex(0);
             function getImgURlForCardIndex($index) {
                 
                 // get a number from 0 to 51
                 // return an image url 
                 
                 $suitIndex = floor($index / 13); 
-                
-                echo "suitIndex: $suitIndex"; 
+                echo "suitIndex:  $suitIndex"; 
             }
             
             function generateDeck() {
@@ -54,9 +72,12 @@
                         'imgURL' => ""
                         ); 
                 }
-            } */
+            } 
             
-            printGameState($allPlayers);
         ?>
+        <form>
+                <input type = "submit" value = "Play Again"/>
+        </form>
     </body>
+    
 </html>
