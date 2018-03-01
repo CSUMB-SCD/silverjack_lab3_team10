67ade01fd3ef818fb4042b63222cade721710623
Raju Kumar <rakumar@csumb.edu>
2018-03-01 19:03:51 +0000

    Added Header Font

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ include 'inc/functions.php';
         <title>SilverJack</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
                     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Graduate" rel="stylesheet">
+
         <link href = "css/styles.css" rel="stylesheet" type="text/css" />
     </head>
     <header>
